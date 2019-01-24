1f56122add9d3de7c9093210805167a766dbf517
Yasemin Göktaş <ygkts61@gmail.com>
2019-01-24 20:14:02 +0300

    ilk yorumu bu olsun

---
+++ y2_disconnectedProje/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace y2_disconnectedProje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonTemizle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BİR MÜŞTERİ SEÇİNİZ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(395, 676);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column6,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView2.Location = new System.Drawing.Point(432, 137);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(612, 585);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(429, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "KARGO FİRMASI SEÇİNİZ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(432, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(612, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(432, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(460, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "SİPARİŞLERİ GETİR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "ID";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "KARGO";
+            this.Column2.Name = "Column2";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "KargoAD";
+            this.Column6.Name = "Column6";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "OrderDate";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "RequiredDate";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "ShippedDate";
+            this.Column5.Name = "Column5";
+            // 
+            // buttonTemizle
+            // 
+            this.buttonTemizle.BackColor = System.Drawing.Color.LightCoral;
+            this.buttonTemizle.Location = new System.Drawing.Point(899, 81);
+            this.buttonTemizle.Name = "buttonTemizle";
+            this.buttonTemizle.Size = new System.Drawing.Size(145, 41);
+            this.buttonTemizle.TabIndex = 4;
+            this.buttonTemizle.Text = "TEMİZLE";
+            this.buttonTemizle.UseVisualStyleBackColor = false;
+            this.buttonTemizle.Click += new System.EventHandler(this.buttonTemizle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(1056, 734);
+            this.Controls.Add(this.buttonTemizle);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Button buttonTemizle;
+    }
+}
+
---
+++ y2_disconnectedProje/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace y2_disconnectedProje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // shipperId'yi combobox'tan alan ve datagrid1 den seçili satıra ait Customer ID ile aynı olan verileri datargiview2 ye yazar. 
+        // ve comboboxtan seçilen shipperID ile butona tıklandığında o ID ye ait siparişler ve tarhiler gelir
+        SqlConnection conn = new SqlConnection("Data Source = YASEMINGOKTAS; Database = NORTHWND; Trusted_Connection=true;");
+        SqlCommand cmd;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            SqlDataAdapter adap = new SqlDataAdapter("Select o.ShipVia, o.CustomerID, c.CompanyName from Shippers s inner join Orders o on s.ShipperID = o.ShipVia inner join Customers c on c.CustomerID=o.CustomerID", conn);
+            DataTable tablo = new DataTable();
+            adap.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            dataGridView1.Columns[0].Visible = false;
+            cmd = new SqlCommand("Select ShipperID from Shippers", conn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                comboBox1.Items.Add(dr[0]);
+            }
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            
+            if (dataGridView1.CurrentCell == null)
+            {
+                cmd = new SqlCommand("Select  o.CustomerID, o.ShipVia, o.ShipName , o.OrderDate, o.RequiredDate, o.ShippedDate from Shippers s inner join Orders o on s.ShipperID = o.ShipVia inner join Customers c on c.CustomerID=o.CustomerID  where o.ShipVia = @sAD", conn);
+                cmd.Parameters.AddWithValue("@sAD", comboBox1.Text);
+                int satir = 0;
+                dataGridView2.Rows.Clear();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    satir = dataGridView2.Rows.Add();
+                    dataGridView2.Rows[satir].Cells[0].Value = dr[0].ToString();
+                    dataGridView2.Rows[satir].Cells[1].Value = dr[1].ToString();
+                    dataGridView2.Rows[satir].Cells[2].Value = dr[2].ToString();
+                    dataGridView2.Rows[satir].Cells[3].Value = dr[3].ToString();
+                    dataGridView2.Rows[satir].Cells[4].Value = dr[4].ToString();
+                    dataGridView2.Rows[satir].Cells[5].Value = dr[5].ToString();
+                }
+            }
+            else 
+            {
+                cmd = new SqlCommand("Select  o.CustomerID, o.ShipVia, o.ShipName, o.OrderDate, o.RequiredDate, o.ShippedDate from Shippers s inner join Orders o on s.ShipperID = o.ShipVia inner join Customers c on c.CustomerID=o.CustomerID  where o.ShipVia = @sAD and o.CustomerID = @cID", conn);
+                cmd.Parameters.AddWithValue("@sAD", comboBox1.Text);
+                cmd.Parameters.AddWithValue("@cID", dataGridView1.CurrentRow.Cells[1].Value.ToString());
+                int satir = 0;
+                dataGridView2.Rows.Clear();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    satir = dataGridView2.Rows.Add();
+                    dataGridView2.Rows[satir].Cells[0].Value = dr[0].ToString();
+                    dataGridView2.Rows[satir].Cells[1].Value = dr[1].ToString();
+                    dataGridView2.Rows[satir].Cells[2].Value = dr[2].ToString();
+                    dataGridView2.Rows[satir].Cells[3].Value = dr[3].ToString();
+                    dataGridView2.Rows[satir].Cells[4].Value = dr[4].ToString();
+                    dataGridView2.Rows[satir].Cells[5].Value = dr[5].ToString();
+                }
+                // dataGridView1.CurrentCell = null;
+            }
+            
+            
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+        private void buttonTemizle_Click(object sender, EventArgs e)
+        {
+            dataGridView1.CurrentCell = null;
+            dataGridView2.Rows.Clear();
+            comboBox1.SelectedItem = null;
+        }        
+    }
+}
